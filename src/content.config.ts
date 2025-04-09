8ccd605b1659b9230f6dad20e3bad76b62af580b
Charlie Carolan <charliecarolan1999@gmail.com>
2025-04-09 21:54:06 +0100

    feat: add games

---
+++ src/content.config.ts
@@ -0,0 +1,19 @@
+import { defineCollection, z } from 'astro:content';
+
+import { glob } from 'astro/loaders'
+
+const games = defineCollection({
+	loader: glob({
+		pattern: '*.json',
+		base: "./src/data/games"
+	}),
+	schema: z.object({
+		games: z.array(z.object({
+			name: z.string(),
+		}))
+	})
+})
+
+export const collections = {
+	games,
+}
